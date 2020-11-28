b63a709a78b7a1d3def8c5ea3f672f1c3f3fe284
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2020-11-28 14:03:36 +0100

    Implement run_map_server

--- IceGauntlet/run_map_server.py
+++ IceGauntlet/run_map_server.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import random
 import Ice
 import json
 Ice.loadSlice('IceGauntlet.ice')
@@ -11,46 +13,84 @@ import IceGauntlet
 # pylint: enable=E0401
 # pylint: enable=C0413
 
-MAPS_DB = "maps.json"
+MAPS_DB = "./maps.json"
+MAPS_FOLDER = "./maps/"
+MAPS_REMOVED_FOLDER = "./removed_maps/"
+
+if len(sys.argv) != 2:
+    print('Usage: ./run_map_server.py <auth_server_proxy>')
+    sys.exit(-1)
 
 class MapServiceI(IceGauntlet.MapService):
     
     def __init__(self, authProxy):
-        # auth_server = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
+        
         self.auth_server = IceGauntlet.AuthenticationPrx.checkedCast(authProxy)
+        self.maps = dict()
+        
         if not auth_server:
             raise RuntimeError('Invalid proxy')
-        
+        with open(MAPS_DB, 'r', encoding='UTF-8') as mapsFileHandler:
+            self.maps = json.load(mapsFileHandler)
     
     def publish(self, token, roomData, current=None):
-        print("{0}: {1}".format(self.n, message))
-        sys.stdout.flush()
-        self.n += 1
-        
-    def remove(self, token, roomName, current=None):
-        if not(auth_server.isValid(token)):
+        if not(self.auth_server.isValid(token)):
             raise IceGauntlet.Unauthorized()
         else:
-            # Leer el JSON 
-            if roomName not in MAPS_DB:   # Llamar a json.load()
+            new_map = json.loads(roomData)
+            if new_map['room'] in self.maps and self.maps[new_map['room']] != token:
+                raise IceGauntlet.RoomAlreadyExists()
+            with open('{0}{1}.{2}'.format(MAPS_FOLDER, new_map['room'], 'json'), 'w', encoding='UTF-8') as newMapFile:
+                json.dump(new_map, newMapFile)
+            self.maps[new_map['room']] = token
+            with open(MAPS_DB, 'w', encoding='UTF-8') as mapsFileHandler:
+                json.dump(self.maps, mapsFileHandler, indent=2)
+             
+        
+    def remove(self, token, roomName, current=None):
+        if not(self.auth_server.isValid(token)):
+            raise IceGauntlet.Unauthorized()
+        else:
+            # Comprobar si el roomName es una de las llaves de maps
+            if roomName not in self.maps:
                 raise IceGauntlet.RoomNotExists()
+            if token == self.maps[roomName]:
+                del self.maps[roomName]
+                os.replace('{0}{1}.{2}'.format(MAPS_FOLDER, roomName, 'json'), '{0}{1}.{2}'.format(MAPS_REMOVED_FOLDER, roomName, 'json'))
+                with open(MAPS_DB, 'w', encoding='UTF-8') as mapsFileHandler:
+                    json.dump(self.maps, mapsFileHandler, indent=2)
+            else:
+                raise IceGauntlet.Unauthorized()
             
-        self.n += 1
 
 class GameServiceI(IceGauntlet.GameService):
+
     def getRoom(self, current=None):
-        print("{0}: {1}".format(self.n, message))
-        sys.stdout.flush()
-        self.n += 1
+        selected_room = None
+        with open(MAPS_DB, 'r', encoding='UTF-8') as mapsFileHandler:
+            maps = json.load(mapsFileHandler)
+            if len(maps.keys()) <= 0:
+                raise IceGauntlet.RoomNotExists()
+            
+            selected_room = random.choice(list(maps.keys()))
+                
+        with open('{0}{1}.{2}'.format(MAPS_FOLDER, selected_room, 'json'), 'r', encoding='UTF-8') as mapFileHandler:
+            roomData = mapFileHandler.read()
+            
+        return roomData
+        
 
 class Server(Ice.Application):
     
     def run(self, argv):
         broker = self.communicator()        #Luego hay que usarlo para el auth_server y conseguir el proxy
-        servant = PrinterI()
+        servant_maps = MapServiceI(broker.stringToProxy(argv[1]))
+        servat_game = GameServiceI()
 
-        adapter = broker.createObjectAdapter("PrinterAdapter")
-        proxy = adapter.add(servant, broker.stringToIdentity("printer1"))
+        adapter_maps = broker.createObjectAdapter("MapsAdapter")
+        adapter_gamme = broker.createObjectAdapter("GameAdapter")
+        proxy = adapter_maps.add(servant_maps, broker.stringToIdentity("Maps1"))
+        proxy = adapter_maps.add(servant_maps, broker.stringToIdentity("Maps1"))
 
         print(proxy, flush=True)
 
@@ -59,7 +99,6 @@ class Server(Ice.Application):
         broker.waitForShutdown()
 
         return 0
-
 
 server = Server()
 sys.exit(server.main(sys.argv))
