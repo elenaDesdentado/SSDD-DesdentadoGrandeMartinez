1ed3360405c1192bd3b665c753c9ab33790a1863
SergioMartinezUCLM <72491012+SergioMartinezUCLM@users.noreply.github.com>
2020-12-03 11:59:52 +0100

    Implement remote version of DungeonMap class

---
+++ IceGauntlet/icegauntlet/game/RemoteDungeonMap.py
@@ -0,0 +1,61 @@
+import sys
+import Ice
+Ice.loadSlice('./IceGauntlet.ice')
+# pylint: disable=E0401
+# pylint: disable=C0413
+import IceGauntlet
+# pylint: enable=E0401
+# pylint: enable=C0413
+
+# class RemoteDungeonMap(Ice.Application):
+    
+    # def __init__(self, game_proxy):
+    #     self.game_proxy = game_proxy
+    #     self._new_room = None
+    #     self.game_server = None
+
+    # def run(self, argv):
+    #     proxy = self.communicator().stringToProxy(self.game_proxy)
+    #     print(self.communicator())
+    #     self.game_server = IceGauntlet.DungeonPrx.checkedCast(proxy)
+    #     if not self.game_server:
+    #         raise RuntimeError('Invalid game proxy')
+    #     self._new_room = self.game_server.getRoom()
+
+    # @property
+    # def next_room(self):
+    #     try:
+    #         #roomData = self.game_server.getRoom()
+    #         self.main(sys.argv)
+    #     except IceGauntlet.RoomNotExists:
+    #         print('[ERROR] No hay ningun mapa subido actualmente')
+    #         sys.exit(-1)
+    #     #return roomData
+    #     return self._new_room
+
+    # @property
+    # def finished(self):
+    #     return True
+    
+class RemoteDungeonMap:
+    def __init__(self, game_proxy, argvs):
+        self.communicator = Ice.initialize(argvs)
+        self.proxy = self.communicator.stringToProxy(game_proxy)
+        self.game_server = IceGauntlet.DungeonPrx.checkedCast(self.proxy)
+        if not self.game_server:
+            raise RuntimeError('Invalid game proxy')
+    
+    @property
+    def next_room(self):
+        try:
+            return self.game_server.getRoom()
+        except IceGauntlet.RoomNotExists:
+            print('[ERROR] No hay ningun mapa subido actualmente')
+            sys.exit(-1)
+
+    @property
+    def finished(self):
+        return True
+    
+    def __del__(self):
+        self.communicator.destroy()
