1edfa780bd0c2fe04b4edab0e2044cfa4e7bd0cb
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2021-01-17 18:44:31 +0100

    Fix bugs detected with the emulator
    The bugs found were fixed. The file where the updates were being added
    was deleted and moved to its proper location.

--- IceGauntlet/run_map_server.py
+++ IceGauntlet/run_map_server.py
@@ -6,7 +6,10 @@ import sys
 import os
 import random
 import json
+import uuid
+import pickle
 from maps_storage import MapsStorage
+import icegauntlettool
 # pylint: disable=import-error
 import Ice
 import IceStorm
@@ -70,9 +73,6 @@ class RoomManagerI(IceGauntlet.RoomManager):
             Remove a map from the server given its name
         '''
         owner = self.auth_server.getOwner(token)
-        # if not self.auth_server.isValid(token):
-        #     raise IceGauntlet.Unauthorized()
-        # else:
         if room_name not in self.maps.get_maps():
             raise IceGauntlet.RoomNotExists()
         if owner == self.maps.get_maps()[room_name]:
@@ -135,10 +135,11 @@ class DungeonI(IceGauntlet.Dungeon):
     '''
         Implements Dungeon interface
     '''
-    def __init__(self, maps_storage):
+    def __init__(self, maps_storage, dungeon_area, adapter):
         self.maps = maps_storage
-        self.adapter = self.communicator().createObjectAdapter("DungeonAreaAdapter")
-        self.dungeon_area = DungeonAreaI("DungeonAreaSyncChannel")
+        self.adapter = adapter
+        self.dungeon_area = dungeon_area
+
     #pylint: disable=invalid-name
     #pylint: disable=unused-argument
     def getEntrance(self, current=None):
@@ -147,36 +148,120 @@ class DungeonI(IceGauntlet.Dungeon):
         '''
             Get a room to be played
         '''
-        selected_room = None
         if len(self.maps.get_maps()) == 0:
             raise IceGauntlet.RoomNotExists()
         else:
-            proxy = adapter.add(self.gungeon_area)
+            proxy = self.adapter.addWithUUID(self.dungeon_area)
             return IceGauntlet.DungeonAreaPrx.checkedCast(proxy)
 
 class DungeonAreaI(IceGauntlet.DungeonArea):
     
-    def __init__(self, topic):
-        self.event_channel = topic
+    item_id = 0
+    
+    def __init__(self, topic_mgr, maps_storage, 
+                 adapter_dungeon_area_sync, adapter_dungeon_area):
+        self.topic_dungeon_area = str(uuid.uuid1())
+        self.topic = topic_mgr
+        self.map_storage = maps_storage
+        try:
+            random_room_name = random.choice(list(maps_storage.get_maps().keys()))
+        except IndexError:
+            print('No hay ningún mapa actualmente almacenado.')
+        self.selected_map = maps_storage.get_map(random_room_name)
+        self.json_content = maps_storage.read_json(random_room_name)
+        self.adapter_dungeon_area_sync = adapter_dungeon_area_sync
+        self.adapter_dungeon_area = adapter_dungeon_area
+        self.topic_areas = topic_mgr.create(self.topic_dungeon_area)
+        self.items = list()
+        self.create_items()
+        self.actors = list()
+        
+        #DungeonAreaSync initialization
+        servant_dungeon_area_sync = DungeonAreaSyncI(self)
+        self.servant_proxy_das = self.adapter_dungeon_area_sync.addWithUUID(
+            servant_dungeon_area_sync)
+        self.topic_areas.subscribeAndGetPublisher({}, self.servant_proxy_das)
+
+        #Subscription of DungeonAreaSync
+        #self.servant_dungeon_area.topic_areas.subscribeAndGetPublisher({}, servant_proxy_das)
+
+        self.next_area = None
+        print(type(self.json_content))
     
     def getEventChannel(self, current=None):
-        return self.event_channel
+        return self.topic_dungeon_area
     
     def getMap(self, current=None):
-        return ''
+        return self.json_content
     
-    def getActor(self, current=None):
-        return 'list of actors'
+    def getActors(self, current=None):
+        return self.actors
     
     def getItems(self, current=None):
-        return 'list of objects'
+        return self.items
     
     def getNextArea(self, current=None):
-        pass
+        if self.next_area is None:
+            self.next_area = DungeonAreaI(self.topic, self.map_storage, 
+                                          self.adapter_dungeon_area_sync, self.adapter_dungeon_area)
+        if len(self.map_storage.get_maps()) == 0:
+            raise IceGauntlet.RoomNotExists()
+        else:
+            proxy = self.adapter_dungeon_area.addWithUUID(self.next_area)
+            return IceGauntlet.DungeonAreaPrx.checkedCast(proxy)
+    
+    def create_items(self):
+        for item in icegauntlettool.get_map_objects(self.json_content):
+            self.items.append(
+                IceGauntlet.Item(str(self.item_id), 
+                                 item[0], item[1][0], item[1][1]))
+            self.item_id += 1
     
 class DungeonAreaSyncI(IceGauntlet.DungeonAreaSync):
+    
+    def __init__(self, dungeon_area):
+        self.dungeon_area = dungeon_area
+    
     def fireEvent(self, event, senderId, current=None):
-        pass
+        try:
+            loaded_event = pickle.loads(event)
+        except pickle.UnpicklingError as e:
+            print('Unexpected error occured')
+            return
+            
+        event_type = loaded_event[0]
+        if event_type == 'spawn_actor':
+            if not loaded_event[1].isnumeric():
+                self.dungeon_area.actors.append(
+                    IceGauntlet.Actor(loaded_event[1], loaded_event[2]))
+        elif event_type == "kill_object":
+            self.kill_object(loaded_event[1])
+        elif event_type == "open_door":
+            door_position = next(filter(lambda item: item.itemId == loaded_event[2], 
+                                        self.dungeon_area.getItems()))
+            items_dict = dict()
+            for item in self.dungeon_area.items:
+                items_dict[item.itemId] = (item.itemType, (item.positionX, item.positionY))
+            doors = icegauntlettool.search_adjacent_door(items_dict, door_position)
+            for door in doors:
+                self.kill_object(door)
+
+    def kill_object(self, id):
+        index = -1
+        if id.isnumeric():
+            object_list = self.dungeon_area.items
+            for i, item in enumerate(object_list):
+                if item.itemId == id:
+                    index = i
+                    break
+        else:
+            object_list = self.dungeon_area.actors
+            for i, actor in enumerate(object_list):
+                if actor.actorId == id:
+                    index = i
+                    break
+        if index > -1:
+            del object_list[index]
 
 class Server(Ice.Application):
     '''
@@ -205,14 +290,11 @@ class Server(Ice.Application):
             return 2
         
         topic_room_manager_sync = "RoomManagerSyncChannel"
-        topic_dungeon_area = "DungeonAreaSyncChannel"
         qos = {}
         try:
             topic = topic_mgr.retrieve(topic_room_manager_sync)
-            topic_areas = topic_mgr.retrieve(topic_dungeon_area)
         except IceStorm.NoSuchTopic:
             topic = topic_mgr.create(topic_mgr.retrieve(topic_room_manager_sync))
-            topic_areas = topic_mgr.retrieve(topic_dungeon_area)
         
         ###### PARTE DEL PUBLISHER #######
         publisher = topic.getPublisher()
@@ -236,18 +318,22 @@ class Server(Ice.Application):
         servant_roomManagerSync.manager_id = broker.identityToString(identity)
         servant_maps.manager_id = broker.identityToString(identity)
 
+        # Subscription of RoomManagerSync
         topic.subscribeAndGetPublisher(qos, servant_proxy)
 
-        #Dungeon initialization
-        servant_game = DungeonI(maps_storage)
-        proxy_game = adapter_maps_game.add(
-            servant_game, broker.stringToIdentity("Game"))
-        
-        #DungeonAreaSync initialization
-        servant_dungeon_area_sync = DungeonAreaSyncI()
+        # DungeonArea initialization
         adapter_dungeon_area_sync = broker.createObjectAdapter(
             "DungeonAreaSyncAdapter")
-        servant_proxy_das = adapter.add(adapter_dungeon_area_sync)
+        dungeon_area_adapter = broker.createObjectAdapter(
+            "DungeonAreaAdapter")
+        servant_dungeon_area = DungeonAreaI(topic_mgr, maps_storage, 
+                                            adapter_dungeon_area_sync, dungeon_area_adapter)
+        
+        #Dungeon initialization
+        servant_game = DungeonI(maps_storage, 
+                                servant_dungeon_area, dungeon_area_adapter)
+        proxy_game = adapter_maps_game.add(
+            servant_game, broker.stringToIdentity("Game"))
         
         room_manager_sync_publisher.hello(casted_proxy_maps, servant_roomManagerSync.manager_id)
 
@@ -256,10 +342,12 @@ class Server(Ice.Application):
         
         adapter_maps_game.activate()
         adapter.activate()
+        adapter_dungeon_area_sync.activate()
+        dungeon_area_adapter.activate()
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
         
-        topic.unsubscribe(servant_proxy)   # sacar suscriber del canal al terminar la ejecución -> el topic va a seguir enviandole cosas anque no exista
+        topic.unsubscribe(servant_proxy)
 
         return 0
 
